bc8661c5f2849c979fc5006b2e7eb29aa477a6ce
Corvus-DDoSKrom <corvusddoskrom@gmail.com>
2022-11-12 20:34:24 -0300

    Actualizando Reporte

---
+++ assets/dompdf/lib/fonts/dompdf_font_family_cache.php
@@ -0,0 +1,97 @@
+<?php return array (
+  'sans-serif' => array(
+    'normal' => $fontDir . '/Helvetica',
+    'bold' => $fontDir . '/Helvetica-Bold',
+    'italic' => $fontDir . '/Helvetica-Oblique',
+    'bold_italic' => $fontDir . '/Helvetica-BoldOblique',
+  ),
+  'times' => array(
+    'normal' => $fontDir . '/Times-Roman',
+    'bold' => $fontDir . '/Times-Bold',
+    'italic' => $fontDir . '/Times-Italic',
+    'bold_italic' => $fontDir . '/Times-BoldItalic',
+  ),
+  'times-roman' => array(
+    'normal' => $fontDir . '/Times-Roman',
+    'bold' => $fontDir . '/Times-Bold',
+    'italic' => $fontDir . '/Times-Italic',
+    'bold_italic' => $fontDir . '/Times-BoldItalic',
+  ),
+  'courier' => array(
+    'normal' => $fontDir . '/Courier',
+    'bold' => $fontDir . '/Courier-Bold',
+    'italic' => $fontDir . '/Courier-Oblique',
+    'bold_italic' => $fontDir . '/Courier-BoldOblique',
+  ),
+  'helvetica' => array(
+    'normal' => $fontDir . '/Helvetica',
+    'bold' => $fontDir . '/Helvetica-Bold',
+    'italic' => $fontDir . '/Helvetica-Oblique',
+    'bold_italic' => $fontDir . '/Helvetica-BoldOblique',
+  ),
+  'zapfdingbats' => array(
+    'normal' => $fontDir . '/ZapfDingbats',
+    'bold' => $fontDir . '/ZapfDingbats',
+    'italic' => $fontDir . '/ZapfDingbats',
+    'bold_italic' => $fontDir . '/ZapfDingbats',
+  ),
+  'symbol' => array(
+    'normal' => $fontDir . '/Symbol',
+    'bold' => $fontDir . '/Symbol',
+    'italic' => $fontDir . '/Symbol',
+    'bold_italic' => $fontDir . '/Symbol',
+  ),
+  'serif' => array(
+    'normal' => $fontDir . '/Times-Roman',
+    'bold' => $fontDir . '/Times-Bold',
+    'italic' => $fontDir . '/Times-Italic',
+    'bold_italic' => $fontDir . '/Times-BoldItalic',
+  ),
+  'monospace' => array(
+    'normal' => $fontDir . '/Courier',
+    'bold' => $fontDir . '/Courier-Bold',
+    'italic' => $fontDir . '/Courier-Oblique',
+    'bold_italic' => $fontDir . '/Courier-BoldOblique',
+  ),
+  'fixed' => array(
+    'normal' => $fontDir . '/Courier',
+    'bold' => $fontDir . '/Courier-Bold',
+    'italic' => $fontDir . '/Courier-Oblique',
+    'bold_italic' => $fontDir . '/Courier-BoldOblique',
+  ),
+  'dejavu sans' => array(
+    'bold' => $fontDir . '/DejaVuSans-Bold',
+    'bold_italic' => $fontDir . '/DejaVuSans-BoldOblique',
+    'italic' => $fontDir . '/DejaVuSans-Oblique',
+    'normal' => $fontDir . '/DejaVuSans',
+  ),
+  'dejavu sans mono' => array(
+    'bold' => $fontDir . '/DejaVuSansMono-Bold',
+    'bold_italic' => $fontDir . '/DejaVuSansMono-BoldOblique',
+    'italic' => $fontDir . '/DejaVuSansMono-Oblique',
+    'normal' => $fontDir . '/DejaVuSansMono',
+  ),
+  'dejavu serif' => array(
+    'bold' => $fontDir . '/DejaVuSerif-Bold',
+    'bold_italic' => $fontDir . '/DejaVuSerif-BoldItalic',
+    'italic' => $fontDir . '/DejaVuSerif-Italic',
+    'normal' => $fontDir . '/DejaVuSerif',
+  ),
+  'font awesome 6 brands' => array(
+    'normal' => $fontDir . '/font_awesome_6_brands_normal_80f28419a8a09a384d23d5de32e12f4f',
+  ),
+  'font awesome 6 free' => array(
+    'normal' => $fontDir . '/font_awesome_6_free_normal_ce2c84d4514dd36f552d0ee56534f77e',
+    '900' => $fontDir . '/font_awesome_6_free_900_f918412b0cea8d76509caa4056f5f640',
+  ),
+  'font awesome 5 brands' => array(
+    'normal' => $fontDir . '/font_awesome_5_brands_normal_80f28419a8a09a384d23d5de32e12f4f',
+  ),
+  'font awesome 5 free' => array(
+    '900' => $fontDir . '/font_awesome_5_free_900_f918412b0cea8d76509caa4056f5f640',
+    'normal' => $fontDir . '/font_awesome_5_free_normal_ce2c84d4514dd36f552d0ee56534f77e',
+  ),
+  'fontawesome' => array(
+    'normal' => $fontDir . '/fontawesome_normal_fe4c4b4157f031625029e35e692dd1eb',
+  ),
+) ?>
--- report.php
+++ report.php
@@ -20,7 +20,7 @@ ob_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HISTORIAL</title>
     <link rel="shortcut icon" href="assets/img/ico.png" type="image/x-icon">
-    <link rel="stylesheet" href="https://github.com/Corvus-DDoSKrom/Policlinico/blob/main/assets/css/report.css">
+    <link rel="stylesheet" href="http://localhost/policlinico/assets/css/report.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;300;400&display=swap');
@@ -28,70 +28,78 @@ ob_start();
 </head>
 <body>
     <p class="titulo">HISTORIAL</p>
+    <p class="tam-label-p">Fecha de consulta: <?php echo $view['fecha']; ?></p>
+    <p class="tam-label-p">Especialidad: <?php echo $view['title']; ?></p>
     <fieldset class="form-9">
         <div>
-            <div>
-                <h2>DATOS DEL PACIENTE</h2>
-            </div>
+            <h2>DATOS DEL PACIENTE</h2>
         </div>
-        <div class="div-4">
-            <div class="div-5">
-                <label class="controls-10" for="nombre">NOMBRE</label>
-                <input class="controls-9" type="text" name="name_patient" id="nombre" value="<?php echo $view['name_patient']; ?>">
-            </div> <!--FIN DE NOMBRE PACIENTE-->
-            <div class="div-5">
-                <label class="controls-10" for="apellido">APELLIDO</label>
-                <input class="controls-9" type="text" name="surname_patient" id="apellido" value="<?php echo $view['surname_patient']; ?>">
-            </div> <!--FIN APELLIDO DELM PACIENTE-->
-            <div class="div-5">
-                <label class="controls-10" for="cedula">CEDULA</label>
-                <input class="controls-9" type="number" name="ci_patient" id="cedula" value="<?php echo $view['ci_patient']; ?>">
-            </div> <!--FIN DE CEDULA PACIENTE-->
-            <div class="div-5">
-                <label class="controls-10" for="age">EDAD</label>
-                <input class="controls-9" type="text" name="age" id="age" value="<?php echo $view['age']; ?>">
-            </div>
-            <div class="div-5">
-                <label class="controls-10" for="sex">SEXO</label>
-                <input class="controls-9" type="text" style="text-transform:uppercase;" name="sex" id="sex" value="<?php echo $view['sex']; ?>">
-            </div>
+        <div>
+            <label class="tam-label" for="nombre">NOMBRE</label>
+            <br>
+            <input class="controls-9" type="text" name="name_patient" id="nombre" value="<?php echo $view['name_patient']; ?>">
         </div>
+        <div>
+            <label class="tam-label" for="apellido">APELLIDO</label>
+            <br>
+            <input class="controls-9" type="text" name="surname_patient" id="apellido" value="<?php echo $view['surname_patient']; ?>">
+        </div>
+        <div>
+            <label class="tam-label" for="cedula">CEDULA</label>
+            <br>
+            <input class="controls-9" type="text" name="ci_patient" id="cedula" value="<?php echo $view['ci_patient']; ?>">
+        </div>
+        <div>
+            <label class="tam-label" for="age">EDAD</label>
+            <br>
+            <input class="controls-9" type="text" name="age" id="age" value="<?php echo $view['age']; ?>">
+        </div>
+        <div>
+            <label class="tam-label" for="sex">SEXO</label>
+            <br>
+            <input class="controls-9" type="text" style="text-transform:uppercase;" name="sex" id="sex" value="<?php echo $view['sex']; ?>">
+        </div>
+        </fieldset>
+        <fieldset class="form-9">
         <div>
             <div>
                 <h2>EXÁMEN FÍSICO - SIGNOS VITALES</h2>
             </div>
         </div>
-        <div class="div-4">
-            <div class="div-5">
-                <label class="controls-10" for="peso">PESO</label>
-                <input class="controls-9" type="text" name="peso_consulta" value="<?php echo $view['peso_consulta']; ?>">
-            </div>
-            <div class="div-5">
-                <label class="controls-10" for="altura">ALTURA</label>
-                <input class="controls-9" type="text" name="altura_consulta" value="<?php echo $view['altura_consulta']; ?>">
-            </div>
-            <div class="div-5">
-                <label class="controls-10" for="presion">PRESIÓN A.</label>
-                <input class="controls-9" type="text" name="presion_consulta" value="<?php echo $view['presion_consulta']; ?>">
-            </div>
+        <div>
+            <label class="tam-label" for="peso">PESO</label>
+            <br>
+            <input class="controls-9" type="text" name="peso_consulta" value="<?php echo $view['peso_consulta']; ?>">
         </div>
-        <div class="div-4">
-            <div class="div-5">
-                <label class="controls-10" for="respiracion">RESPIRACIÓN</label>
-                <input class="controls-9" type="text" name="respiracion_consulta" value="<?php echo $view['respiracion_consulta']; ?>">
-            </div>
-            <div class="div-5">
-                <label class="controls-10" for="pulso">FREC. C.</label>
-                <input class="controls-9" type="text" name="pulso_consulta" value="<?php echo $view['pulso_consulta']; ?>">
-            </div>
-            <div class="div-5">
-                <label class="controls-10" for="temperatura">TEMPERATURA</label>
-                <input class="controls-9" type="text" name="temperatura_consulta" value="<?php echo $view['temperatura_consulta']; ?>">
-            </div>
+        <div>
+            <label class="tam-label" for="altura">ALTURA</label>
+            <br>
+            <input class="controls-9" type="text" name="altura_consulta" value="<?php echo $view['altura_consulta']; ?>">
+        </div>
+        <div>
+            <label class="tam-label" for="presion">PRESIÓN A.</label>
+            <br>
+            <input class="controls-9" type="text" name="presion_consulta" value="<?php echo $view['presion_consulta']; ?>">
+        </div>
+        <div>
+            <label class="tam-label" for="respiracion">RESPIRACIÓN</label>
+            <br>
+            <input class="controls-9" type="text" name="respiracion_consulta" value="<?php echo $view['respiracion_consulta']; ?>">
+        </div>
+        <div>
+            <label class="tam-label" for="pulso">FREC. C.</label>
+            <br>
+            <input class="controls-9" type="text" name="pulso_consulta" value="<?php echo $view['pulso_consulta']; ?>">
+        </div>
+        <div>
+            <label class="tam-label" for="temperatura">TEMPERATURA</label>
+            <br>
+            <input class="controls-9" type="text" name="temperatura_consulta" value="<?php echo $view['temperatura_consulta']; ?>">
         </div>
     </fieldset>
-    <fieldset class="form-9" style="margin-bottom: 15px;">
+    <fieldset class="form-9">
         <div>
+            <h2>HISTORIAL DEL PACIENTE</h2>
             <label class="tam-label" for="motivo">MOTIVO DE CONSULTA</label>
             <br>
             <textarea rows="4" class="controls-11" style="resize: none;" name="motivo_consulta"><?php echo $view['motivo_consulta']; ?></textarea>
@@ -106,6 +114,8 @@ ob_start();
             <br>
             <textarea rows="4" class="controls-11" style="resize: none;" name="diagnostico"><?php echo $view['diagnostico']; ?></textarea>
         </div>
+        </fieldset>
+        <fieldset class="form-9">
         <div>
             <h2>MEDIO AUXILIAR DE DIAGNOSTICO</h2>
             <label class="tam-label" for="auxiliar">DIAGNOSTICO AUXILIAR</label>
@@ -122,15 +132,15 @@ ob_start();
             <br>
             <textarea rows="4" class="controls-11" style="resize: none;" name="tratamiento_consulta"><?php echo $view['tratamiento_consulta']; ?></textarea>
         </div>
-        <div class="div-4">
-            <div class="div-5">
-                <label class="controls-10" for="cie10">CIE10</label>
-                <input class="controls-9" type="text" name="cie10_consulta" value="<?php echo $view['cie10_consulta']; ?>">
-            </div>
-            <div class="div-5">
-                <label class="controls-10" for="proxima">PROXIMA CONSULTA</label>
-                <input class="controls-2" type="text" name="proxima_consulta" value="<?php echo $view['fecha']; ?>">
-            </div>
+        <div>
+            <label class="tam-label" for="cie10">CIE10</label>
+            <br>
+            <input class="controls-9" type="text" name="cie10_consulta" value="<?php echo $view['cie10_consulta']; ?>">
+        </div>
+        <div>
+            <label class="tam-label" for="proxima">PROXIMA CONSULTA</label>
+            <br>
+            <input class="controls-9" type="text" name="proxima_consulta" value="<?php echo $view['proxima_consulta']; ?>">
         </div>
     </fieldset>
 </body>
@@ -143,7 +153,7 @@ use Dompdf\Dompdf;
 $dompdf = new dompdf();
 
 $options = $dompdf->getOptions();
-$options->set(array('isTemoteEnabled' => true));
+$options->set(array('isRemoteEnabled' => true));
 $dompdf->setOptions($options);
 
 $dompdf->loadhtml($html);
