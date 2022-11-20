5e8c1eb07ad7135556e5ffb03a3282090b6135ea
Bowchers Rey <89953021+Bowchers@users.noreply.github.com>
2022-11-19 21:26:26 -0300

    Update patient.php

--- patient.php
+++ patient.php
@@ -28,8 +28,9 @@ mysqli_close($conn);
 <body>
     <!-- Menu -->
     <div class="nav-bar">
-        <a class="logo" href="agenda"><img class="site-logo" src="assets/img/logo.png" alt=""></a>
+        <a class="logo" href="panel_reception"><img class="site-logo" src="assets/img/logo.png" alt=""></a>
         <nav class="menuhorizontal">
+            <a href="panel_reception"><i class="fa-solid fa-house-medical"></i> Inicio</a> 
             <a href="agenda"><i class="fa-solid fa-calendar-days"></i> Agenda</a>
             <a href="register_patient"><i class="fa-solid fa-person-half-dress"></i> Registrar Paciente</a>
             <a href="patient"><i class="fa-solid fa-person-half-dress"></i> Paciente</a>
@@ -66,7 +67,6 @@ mysqli_close($conn);
                                 <th scope="col">FECHA DE NACIMIENTO</th>
                                 <th scope="col">NACIONALIDAD</th>
                                 <th scope="col">RESIDENCIA</th>
-                                <th scope="col">BARRIO</th>
                                 <th scope="col">ESTADO CIVIL</th>
                                 <th scope="col">EDAD</th>
                                 <th scope="col">SEXO</th><
@@ -82,7 +82,7 @@ mysqli_close($conn);
                         </tfoot>
                         <tbody id="datos">
                             
-                            <?php /* Este codigo recorre los datos de la tabla de la base de datos*/
+                            <?php /*Este codigo recorre los datos de la tabla de la base de datos*/
                             foreach($result as $row){
                             ?>
                             <tr>
@@ -93,7 +93,6 @@ mysqli_close($conn);
                                 <td><?php echo $row['date_of_birth'];?></td>
                                 <td><?php echo $row['nationality'];?></td>
                                 <td><?php echo $row['residence'];?></td>
-                                <td><?php echo $row['barrio'];?></td>
                                 <td><?php echo $row['marital_status'];?></td>
                                 <td><?php echo $row['age'];?></td>
                                 <td><?php echo $row['sex'];?></td>
